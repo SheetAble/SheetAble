a6a2c3011ddc7c909ca810d33b5596b9658ff24d
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-12 11:06:56 +0200

    Change types

--- backend/api/models/Division.go
+++ backend/api/models/Division.go
@@ -11,15 +11,15 @@ import (
 )
 
 type Division struct {
-	Name      string    `gorm:"primary_key" json:"name"`
-	Division  string    `json:division`
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	Name         string    `gorm:"primary_key" json:"name"`
+	DivisionName string    `json:"division_name"`
+	CreatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 func (d *Division) Prepare() {
 	d.Name = html.EscapeString(strings.TrimSpace(d.Name))
-	d.Division = d.Division
+	d.DivisionName = html.EscapeString(strings.TrimSpace(d.DivisionName))
 	d.CreatedAt = time.Now()
 	d.UpdatedAt = time.Now()
 }
