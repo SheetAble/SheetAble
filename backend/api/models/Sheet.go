ed7add280413cc9d09631ac28c71c896929cfba5
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-18 18:23:43 +0200

    Add GetAllSheets method and change variable names

--- backend/api/models/Sheet.go
+++ backend/api/models/Sheet.go
@@ -12,32 +12,35 @@ import (
 
 type Sheet struct {
 	SheetName   string `gorm:"primary_key" json:"sheet_name"`
-	Author      User   `json:"author"`
+	Composer    string `json:"composer"`
 	ReleaseDate time.Time
-	AuthorID    uint32    `gorm:"not null" json:"author_id"`
+	UploaderID  uint32    `gorm:"not null" json:"uploader_id"`
 	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 func (s *Sheet) Prepare() {
 	s.SheetName = html.EscapeString(strings.TrimSpace(s.SheetName))
-	s.Author = User{}
-	s.ReleaseDate = s.ReleaseDate
+	s.Composer = html.EscapeString(strings.TrimSpace(s.Composer))
 	s.CreatedAt = time.Now()
 	s.UpdatedAt = time.Now()
 }
 
 func (s *Sheet) SaveSheet(db *gorm.DB) (*Sheet, error) {
-	var err error
-	err = db.Debug().Model(&Sheet{}).Create(&s).Error
+	err := db.Debug().Model(&Sheet{}).Create(&s).Error
 	if err != nil {
 		return &Sheet{}, err
 	}
-	if s.SheetName != "" {
-		err = db.Debug().Model(&User{}).Where("id = ?", s.AuthorID).Take(&s.Author).Error
-		if err != nil {
-			return &Sheet{}, err
-		}
-	}
 	return s, nil
+}
+
+func (s *Sheet) GetAllSheets(db *gorm.DB) (*[]Sheet, error) {
+	// This method will return max 20 sheets, to find more or specific one you need to specify it
+	var err error
+	sheets := []Sheet{}
+	err = db.Debug().Model(&Sheet{}).Limit(20).Find(&sheets).Error
+	if err != nil {
+		return &[]Sheet{}, err
+	}
+	return &sheets, err
 }
