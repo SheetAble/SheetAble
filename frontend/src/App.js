b1c510af7736db4c7dfbd3978333d50f5a748f1f
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-22 18:10:55 +0200

    Add token check on compile of application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,39 @@ import HomePage from "./Components/Home/HomePage";
 // Redux
 import { Provider } from 'react-redux'
 import store from './Redux/store';
+import { logoutUser, getUserData } from './Redux/Actions/userActions'
+import { SET_AUTHENTICATED } from './Redux/types'
+
 
 // Axios
 import axios from 'axios';
 
+// JWT
+import jwtDecode from 'jwt-decode'
+
+
 axios.defaults.baseURL = "http://localhost:8080"
 
+
+// Token stuff 
+// TODO: rewrite into function
+const token = localStorage.FBIdToken
+if(token){
+  const decodedToken = jwtDecode(token)
+  const ts = Date.now()
+  const currentTime = Math.floor(ts/1000) - 7200
+  if(decodedToken.exp < currentTime){
+    store.dispatch(logoutUser())
+    window.location.href = '/login'
+  }
+  else {
+    store.dispatch({ type: SET_AUTHENTICATED })
+    axios.defaults.headers.common['Authorization'] = token
+    // store.dispatch(getUserData())
+  }
+}
+
+// TODO: add an else state of the home page (if not logged in)
 
 function App() {
   return (
