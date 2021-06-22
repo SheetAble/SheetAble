bdd7d279779c75b3bb7f95eb5b778629a49e23a7
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-22 19:26:25 +0200

    Add start of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 // React Router Import
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Pages
+// Pages and Components
 import LoginPage from "./Components/Authentication/LoginPage";
 import SignupPage from "./Components/Authentication/SignupPage";
 import HomePageProvider from "./Components/Home/HomePageProvider";
+import Navbar from './Components/Navbar/Navbar';
 
 // Redux
 import { Provider } from 'react-redux'
@@ -37,7 +38,6 @@ if(token){
   else {
     store.dispatch({ type: SET_AUTHENTICATED })
     axios.defaults.headers.common['Authorization'] = token
-    // store.dispatch(getUserData())
   }
 }
 
@@ -45,6 +45,7 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
+        <Navbar />
         <Switch>
             <Route exact path="/" component={HomePageProvider} />
             <Route exact path="/login" component={LoginPage} />
