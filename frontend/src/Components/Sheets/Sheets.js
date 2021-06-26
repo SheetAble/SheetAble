0206e01a1c81b498bbe4b6646d02e5c4f6edf2c0
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-26 19:27:42 +0200

    Put div in list item

--- frontend/src/Components/Sheets/Sheets.js
+++ frontend/src/Components/Sheets/Sheets.js
@@ -6,18 +6,20 @@ import './Sheet.css'
 
 function Sheets(props) {
 	const { sheets } = props
-	
+
 	const sheetItems = sheets.map((sheet) => {
 		return (
-			<div className="box-container ripple">
-				<img className="thumbnail-image" src={`http://localhost:8080/sheet/thumbnail/${sheet.sheet_name}`} alt="image" />
-				<div className="sheet-name-container">
-					<span className="sheet-name">{sheet.sheet_name}</span>
+			<li key={sheet.sheet_name}>
+				<div className="box-container ripple">
+					<img className="thumbnail-image" src={`http://localhost:8080/sheet/thumbnail/${sheet.sheet_name}`} alt="image" />
+					<div className="sheet-name-container">
+						<span className="sheet-name">{sheet.sheet_name}</span>
+					</div>
+					<div className="sheet-composer-container">
+						<span className="sheet-composer">{sheet.composer}</span>
+					</div>
 				</div>
-				<div className="sheet-composer-container">
-					<span className="sheet-composer">{sheet.composer}</span>
-				</div>
-			</div>
+			</li>
 		)
 	}
  	
@@ -29,5 +31,7 @@ function Sheets(props) {
 		</ul>
 	)
 }
+
+
 
 export default Sheets
