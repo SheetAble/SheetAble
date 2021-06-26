22d1c497599809b8428ed3aa832175a989e96236
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-26 16:19:02 +0200

    Add 401 check

--- frontend/src/Redux/Actions/dataActions.js
+++ frontend/src/Redux/Actions/dataActions.js
@@ -1,19 +1,29 @@
 import { LOADING_DATA, SET_SHEETS} from '../types'
 import axios from 'axios'
 
+import store from '../store';
+import { logoutUser } from './userActions'
+
 // Get all Sheets
 export const getSheets = () => dispatch => {
     dispatch({ type: LOADING_DATA })
     axios.get('/sheets')
         .then(res => {
-			console.log(res.data);
+			
+            console.log(res.data);
+            
             dispatch({
                 type: SET_SHEETS,
                 payload: res.data
             })
         })
         .catch(err => {
+            if (err.request.status == 401) {
+                store.dispatch(logoutUser())
+                window.location.href = '/login'
+            }
             console.log(err)
+            
             dispatch({
                 type: SET_SHEETS,
                 payload: []
