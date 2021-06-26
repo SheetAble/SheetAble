34f47e15e375f6aa8f5d3c461134ff5238fe7c94
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-26 19:59:54 +0200

    Add no available sheets page

--- frontend/src/Components/Home/HomePage.js
+++ frontend/src/Components/Home/HomePage.js
@@ -14,26 +14,43 @@ import './HomePage.css'
 // Components
 import Sheets from '../Sheets/Sheets'
 
+// Images
+import lostImage from '../../Images/lost.svg'
+
 class HomePage extends Component {
     
     componentDidMount = () => {
         this.props.getSheets()
     }
 
+    
             
     render() {
-        return (
-            <Fragment>
-                <SideBar history={this.props.history}/>
-                <div className="home_content">
-			        <div> 
-                        <br />
+        const sheetsTrue = (
+            <div className="home_content">
+                <div> 
+                    <br />
                         <span className="text">Recently Added Sheets</span>
                         <hr className="seperator"></hr>
                         <Sheets sheets={this.props.sheets} />
                     </div>
-                    
-		        </div>
+            </div>
+        )
+        
+        const sheetsFalse = (
+            <div className="home_content">
+                <div className="lost-image-container">
+                    <span className="lost-text-header">Whooops!</span>
+                    <span className="lost-text">Seems like you haven't uploaded any sheets yet.</span> 
+                    <img src={lostImage} className="lost-image" />
+                </div>
+            </div>
+        )
+        return (
+            <Fragment>
+                <SideBar history={this.props.history}/>
+                {this.props.sheets.length > 0 ? sheetsTrue : sheetsFalse}
+                
             </Fragment>
         )
     }	
