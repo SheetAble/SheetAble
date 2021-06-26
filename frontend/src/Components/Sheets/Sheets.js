f832b55ebdac130f0043995d4f9507e96616efdc
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-26 16:30:51 +0200

    Add ripple effect and make margin bigger

--- frontend/src/Components/Sheets/Sheets.js
+++ frontend/src/Components/Sheets/Sheets.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react'
 import './Sheet.css'
 
 
+
 function Sheets(props) {
 	const { sheets } = props
 	
 	const sheetItems = sheets.map((sheet) => {
 		return (
-			<div className="box-container">
+			<div className="box-container ripple">
 				<img className="thumbnail-image" src={`http://localhost:8080/sheet/thumbnail/${sheet.sheet_name}`} alt="image" />
 				<div className="sheet-name-container">
 					<span className="sheet-name">{sheet.sheet_name}</span>
