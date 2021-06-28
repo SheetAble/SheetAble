c40cdd46b7bbcef3b90bce1c0b415fcfc57db64c
Valentin Zwerschke <v.zwerschke@mail.de>
2021-06-28 05:40:46 +0200

    Add new values

--- frontend/src/Components/Composers/Composers.js
+++ frontend/src/Components/Composers/Composers.js
@@ -4,17 +4,17 @@ import './Composers.css'
 
 function Composers(props) {
 	const { composers } = props
-
+	console.log(composers);
 	const composerItems = composers.map((composer) => {
 		return (
 			<li key={composer.name}>
 				<div className="box-container-comp ripple">
-					<img className="thumbnail-image-comp" src="https://assets.openopus.org/portraits/72753742-1568084874.jpg" alt="image" />
+					<img className="thumbnail-image-comp" src={composer.portrait_url} alt="image" />
 					<div className="comp-name-container">
 						<span className="comp-name">{composer.name}</span>
 					</div>
 					<div className="life-container">
-						<span className="life-text">Romantic</span>
+						<span className="life-text">{composer.epoch}</span>
 					</div>
 				</div>
 			</li>
