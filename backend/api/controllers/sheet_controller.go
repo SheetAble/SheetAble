01e0e54ab6bb7d48d5b959f5285c1e927e0fa65c
Valentin Zwerschke <v.zwerschke@mail.de>
2021-07-14 21:03:32 +0200

    Change comments

--- backend/api/controllers/sheet_controller.go
+++ backend/api/controllers/sheet_controller.go
@@ -9,7 +9,10 @@ import (
 )
 
 func (server *Server) GetSheets(w http.ResponseWriter, r *http.Request) {
-	// This endpoint will return max 20 sheets
+	/*
+		This endpoint will return max 20 sheets
+	*/
+
 	sheet := models.Sheet{}
 
 	sheets, err := sheet.GetAllSheets(server.DB)
@@ -20,8 +23,18 @@ func (server *Server) GetSheets(w http.ResponseWriter, r *http.Request) {
 	responses.JSON(w, http.StatusOK, sheets)
 }
 
+func (server *Server) GetSheet(w http.ResponseWriter, r *http.Request) {
+	/*
+		Get PDF file and information about an individual sheet
+	*/
+
+}
+
 func (server *Server) GetThumbnail(w http.ResponseWriter, r *http.Request) {
-	// Serve the thumbnail file
+	/*
+		Serve the thumbnail file
+	*/
+
 	name := mux.Vars(r)["name"]
 	http.ServeFile(w, r, "thumbnails/"+name+".png")
 }
@@ -30,6 +43,7 @@ func (server *Server) GetComposers(w http.ResponseWriter, r *http.Request) {
 	/*
 		Get authors, limited by 20 and sorted by newest
 	*/
+
 	composer := models.Composer{}
 
 	composers, err := composer.GetAllComposer(server.DB)
