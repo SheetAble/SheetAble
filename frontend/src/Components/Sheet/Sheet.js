bef05983feff793dcab2144aeda3d2e7d6c8ae87
Valentin Zwerschke <v.zwerschke@mail.de>
2021-07-14 20:59:33 +0200

    Import sidebar to individual sheet

--- frontend/src/Components/Sheet/Sheet.js
+++ frontend/src/Components/Sheet/Sheet.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Fragment } from 'react';
 
 import { useParams } from "react-router-dom";
 
+import SideBar from '../Navbar/SideBar'
 
 function Sheet() {
 	let { sheetName } = useParams();
 
 	return (
-		<div>
-			{sheetName}	
-		</div>
+		 <Fragment>
+			<SideBar />
+			<div className="home_content">
+				{sheetName}	
+			</div>
+		</Fragment>            
 	)
 }
 
