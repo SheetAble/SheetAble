549ec2c7978e56b420d530a0139a14e10faf00d9
Valentin Zwerschke <v.zwerschke@mail.de>
2021-07-22 18:18:14 +0200

    Fix non thumbnail creation error

--- backend/api/utils/pdfToImage.go
+++ backend/api/utils/pdfToImage.go
@@ -7,7 +7,7 @@ import (
 
 // POST request onto pdf creation
 func RequestToPdfToImage(path string, name string) bool {
-	_, err := http.PostForm("http://localhost:5000/createthumbnail",
+	_, err := http.PostForm("http://127.0.0.1:5000/createthumbnail",
 		url.Values{"path": {path}, "name": {name}})
 
 	return err == nil
