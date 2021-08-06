fc1237d094b7db64e70919bd8943a0b5859f9796
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-06 21:41:17 +0200

    Change box shadow and page navigation position

--- frontend/src/Components/Sheet/Sheet.js
+++ frontend/src/Components/Sheet/Sheet.js
@@ -121,7 +121,7 @@ function Sheet() {
 						<div className="video_player">
 							<span className="coming_soon">Video Player Coming Soon</span>
 							<div>
-								<span><a href="/newsletter">Sign up</a> for the newsletter, so you don't miss any updates</span>
+								<span><a href="/newsletter" target="_blank">Sign up</a> for the newsletter, so you don't miss any updates</span>
 							</div>
 						</div>				
 					</div>											
