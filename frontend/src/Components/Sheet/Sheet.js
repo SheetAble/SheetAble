32f7a0cb83bc5f4bdcb1945a3526ee8966c08101
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-07 17:37:49 +0200

    Change media player card text

--- frontend/src/Components/Sheet/Sheet.js
+++ frontend/src/Components/Sheet/Sheet.js
@@ -119,7 +119,7 @@ function Sheet() {
 						</div>
 
 						<div className="video_player">
-							<span className="coming_soon">Video Player Coming Soon</span>
+							<span className="coming_soon">Media Player Coming Soon</span>
 							<div>
 								<span><a href="/newsletter" target="_blank">Sign up</a> for the newsletter, so you don't miss any updates</span>
 							</div>
