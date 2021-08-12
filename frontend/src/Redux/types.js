7386b33c34441573679f61370095d5ab4a5fef14
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-12 13:23:10 +0200

    Change comment

--- frontend/src/Redux/types.js
+++ frontend/src/Redux/types.js
@@ -1,4 +1,4 @@
-// User reducer types
+// Authentication/User reducer types
 export const SET_AUTHENTICATED = 'SET_AUTHENTICATED'
 export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED'
 // UI reducer types
