ff86bc7431cbcf9d43029dfd08777894e4349ca4
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-16 13:23:21 +0200

    Add new template

--- docs/js/scripts.js
+++
@@ -1,7 +0,0 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.4 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
