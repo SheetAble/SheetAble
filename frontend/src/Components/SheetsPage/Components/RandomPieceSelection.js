feb7c77a735e0463ae39207e27e0c5ee9186c651
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-21 15:10:44 +0200

    Add redirection

--- frontend/src/Components/SheetsPage/Components/RandomPieceSelection.js
+++ frontend/src/Components/SheetsPage/Components/RandomPieceSelection.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import LoadingAnimation from '../../../Images/Animations/Loading.svg'
 import SheetBox from './SheetBox';
 import './BubblyButton.css'
+import { useHistory } from 'react-router';
 
 function RandomPieceSelection({ sheetPages, page }) {
 	
@@ -43,18 +44,22 @@ function RandomPieceSelection({ sheetPages, page }) {
 		},700);
 	};
 
+	let history = useHistory()
+
 	return (	
 		<div className="box rand-piece">
 
 			{loading? <img className="loading-animation-rand" src={LoadingAnimation}/> :
 			(
 				<div>
+					<div onClick={() => history.push(`sheet/${sheet.pdf_url.split("pdf/").pop()}`)} className="cursor">
 						<img className="rand-img" src={`http://localhost:8080/sheet/thumbnail/${sheet.sheet_name}`} alt="image" />
 						<div className="sheet-name-container">
 							<span className="sheet-name">{sheet.sheet_name}</span>
 						</div>
 						<div className="sheet-composer-container">
 							<span className="sheet-composer">{sheet.composer}</span>
+						</div>
 					</div>
 					<button onClick={animateButton} className={bubblyButton}>
 						Shuffle
