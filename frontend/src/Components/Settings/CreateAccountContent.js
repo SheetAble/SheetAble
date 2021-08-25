00c59c93d0dad26b205ec9a3485feb1f2fe6fe91
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-25 14:17:30 +0200

    Add Create Account Modal

---
+++ frontend/src/Components/Settings/CreateAccountContent.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+function CreateAccountContent() {
+
+	const [disabled, setDisabled] = useState(true)
+
+	const [requestData, setRequestData] = useState({
+		email: "",
+		password: "",
+		confirmPassword: "",
+		lastFocus: 0
+	})
+
+	const handleChange = (event) => {
+		
+		setRequestData({
+			...requestData,
+			[event.target.name]: event.target.value,
+			lastFocus: [event.target.name]
+		})    
+		console.log(requestData);	
+    }
+
+	const sendRequest = () => {
+		
+	}
+
+	return (
+		<div className="upload settings">
+			<form noValidate autoComplete="off">
+				<TextField id="standard-basic" error={!re.test(requestData.email) && requestData.email != "" && requestData.lastFocus != "email"} type="email" label="E-Mail" className="form-field" name="email" onChange={handleChange} helperText={!re.test(requestData.email) && requestData.email && requestData.lastFocus != "email" !="" ? "Email address is not valid" : ""}/>
+				<TextField id="standard-password-input" type="password" label="Password" className="form-field comp" name="password" autoComplete="new-password" onChange={handleChange}/>
+				<TextField id="standard-password-input" error={requestData.password != requestData.confirmPassword && requestData.confirmPassword != ""} type="password" label="Confirm Password" className="form-field comp" name="confirmPassword" autoComplete="new-password" helperText={requestData.password != requestData.confirmPassword && requestData.confirmPassword != "" ?  "Passwords do not match." : ""} onChange={handleChange}/>	
+			</form>
+			<Button variant="contained" color="primary" disabled={requestData.email == "" || requestData.password == "" || requestData.confirmPassword != requestData.password} onClick={sendRequest}>
+				Create Account
+			</Button>
+		</div>
+	)
+}
+
+export default CreateAccountContent
--- frontend/src/Components/Settings/Settings.js
+++ frontend/src/Components/Settings/Settings.js
@@ -1,25 +1,59 @@
-import React, { Fragment } from 'react'
-
+import React, { Fragment, useState } from 'react'
 import SideBar from '../Sidebar/SideBar'
-
+import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux'
+import './Settings.css'
+import Modal from '../Sidebar/Modal/Modal'
+import { Button } from '@material-ui/core';
+import CreateAccountContent from './CreateAccountContent';
+
+
 
 function Settings(props) {
+
+	const [admin, setAdmin] = useState(props.userData.id == 1)
+
+	const [modal, setModal] = useState(false)
+
 	return (
 		<Fragment>
 			<SideBar />
-			<div className="home_content">
-				{props.userData.id == 1? 
-				<p>yes admin</p>
-				
-				:
-				
-				<p>No admin</p>
-				}
+			<div className="home_content settings">
+
+				<div className="doc_header">
+					<span className="doc_sheet">Settings</span>
+					<br />
+					<span className="doc_composer ">Account Status: <b>{admin? "Admin" : "Non Admin"}</b></span>
+				</div>
+				<button onClick={() => setModal(!modal)}>Open Modal</button>
+
+				<Modal title="Create New Account" onClose={() => setModal(false)} show={modal}>
+					<CreateAccountContent />
+      			</Modal>
 			</div>
 		</Fragment>
 	)
 }
+
+/*
+	<form noValidate autoComplete="off">
+						<TextField id="standard-basic" label="E-mail" name="E-Mail" />
+						<br />
+						<TextField
+							id="standard-password-input"
+							label="Password"
+							type="password"
+							autoComplete="new-password"
+						/>
+						<br />
+						<TextField
+							id="standard-password-input"
+							label="Confirm Password"
+							type="password"
+							autoComplete="new-password"
+						/>
+					</form>
+*/
 
 const mapStateToProps = (state) => ({
     userData: state.user.userData,
