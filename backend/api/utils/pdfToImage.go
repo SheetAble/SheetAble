d9735284ae033003487a27932d70cedf055e5ac0
Valentin Zwerschke <v.zwerschke@mail.de>
2021-08-31 15:14:45 +0200

    Add new png server

--- backend/api/utils/pdfToImage.go
+++ backend/api/utils/pdfToImage.go
@@ -1,47 +1,119 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
 	"os"
-
-	"github.com/h2non/bimg"
+	"strings"
 )
 
 // POST request onto pdf creation
-/*func RequestToPdfToImage(path string, name string) bool {
-	_, err := http.PostForm("http://127.0.0.1:5000/createthumbnail",
-		url.Values{"path": {path}, "name": {name}})
-
-	return err == nil
-}
-*/
-
 func RequestToPdfToImage(path string, name string) bool {
-	buffer, err := bimg.Read(path)
+	/*_, err := http.PostForm("http://127.0.0.1:5000/createthumbnail",
+	url.Values{"path": {path}, "name": {name}})
+	*/
+	sendRequest(path, name, "http://127.0.0.1:5000/createthumbnail")
+
+	return true
+}
+
+func sendRequest(path string, name string, remoteURL string) {
+
+	var client *http.Client
+	{
+		//setup a mocked http client.
+		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b, err := httputil.DumpRequest(r, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%s", b)
+		}))
+		defer ts.Close()
+		client = ts.Client()
+		//	remoteURL = ts.URL
+	}
+
+	//prepare the reader instances to encode
+	values := map[string]io.Reader{
+		"file": mustOpen(path), // lets assume its this file
+		"name": strings.NewReader(name),
+	}
+	err := Upload(client, remoteURL, values, name)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		panic(err)
+	}
+}
+
+func Upload(client *http.Client, url string, values map[string]io.Reader, name string) (err error) {
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	for key, r := range values {
+		var fw io.Writer
+		if x, ok := r.(io.Closer); ok {
+			defer x.Close()
+		}
+		// Add an image file
+		if x, ok := r.(*os.File); ok {
+			if fw, err = w.CreateFormFile(key, x.Name()); err != nil {
+				return
+			}
+		} else {
+			// Add other fields
+			if fw, err = w.CreateFormField(key); err != nil {
+				return
+			}
+		}
+		if _, err = io.Copy(fw, r); err != nil {
+			return err
+		}
 
 	}
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
 
-	image, err := bimg.NewImage(buffer).Convert(bimg.JPEG)
+	// Now that you have a form, you can submit it to your handler.
+	req, err := http.NewRequest("POST", url, &b)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	// Don't forget to set the content type, this will contain the boundary.
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Submit the request
+	res, err := client.Do(req)
+	if err != nil {
+		return
 	}
 
-	scaleFactor := 2.5 // To scale up the size of the JPG
-
-	// first force resize
-	newImage, errF := bimg.NewImage(image).ForceResize(int(152*scaleFactor), int(221*scaleFactor))
-	if errF != nil {
-		fmt.Fprintln(os.Stderr, err)
+	// Check the response
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("bad status: %s", res.Status)
 	}
 
-	if bimg.NewImage(newImage).Type() == "jpeg" {
-		fmt.Fprintln(os.Stderr, "The image was converted into jpeg")
+	// Save response
+	defer res.Body.Close()
+	out, err := os.Create("thumbnails/" + name + ".png")
+	if err != nil {
+		return
 	}
+	defer out.Close()
+	io.Copy(out, res.Body)
 
-	imgPath := "thumbnails/" + name + ".jpg"
+	return
+}
 
-	bimg.Write(imgPath, newImage)
-	return err == nil
+func mustOpen(f string) *os.File {
+	r, err := os.Open(f)
+	if err != nil {
+		panic(err)
+	}
+	return r
 }
---
+++ pdf2png server/app.py
@@ -0,0 +1,55 @@
+'''
+    This server is created because transforming a pdf to an image doesn't work properly in Go (currently),
+    so I had to create a small flask server for that.
+'''
+
+from flask import Flask
+from flask import request
+from flask import send_file
+from flask import after_this_request
+
+import os
+from pdf2image import convert_from_path
+
+
+app = Flask(__name__)
+
+'''
+    POST request onto /createthumbnail with form data
+    Form Data:
+    file: ("your pdf file")
+    name: "your sheet name"
+'''
+
+@app.route("/createthumbnail", methods=['POST'])
+def index():
+    print("in request")
+    f = request.files['file']
+    name = request.form['name']
+    f.save(name + '.pdf')    
+
+    createThumbnail(name)
+
+    @after_this_request
+    def cleanup(response):
+        os.remove(f"./{name}.pdf")
+        os.remove(f"./thumbnails/{name}.png") 
+        return response
+
+    return send_file(f'./thumbnails/{name}.png', attachment_filename='python.jpg')
+
+  
+
+def createThumbnail(name):
+    createImg(f"./{name}.pdf", name)
+    return 
+
+def createImg(path, name):
+    scale_factor = 2.5 # To scale up the size of the PNG
+    pages = convert_from_path(path, single_file=True, size=(152 * scale_factor, 214 * scale_factor))
+
+    pages[0].save(f'./thumbnails/{name}.png', 'PNG')
+
+
+if __name__ == '__main__':
+    app.run()
