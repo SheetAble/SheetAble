48d98c04e72fa98018174616b782b19f0915a0e3
Valentin Zwerschke <v.zwerschke@mail.de>
2021-09-05 21:53:55 +0200

    Make token expiration after 1 week

--- backend/api/auth/token.go
+++ backend/api/auth/token.go
@@ -17,7 +17,7 @@ func CreateToken(user_id uint32) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["user_id"] = user_id
-	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	claims["exp"] = time.Now().Add(time.Hour * 168).Unix() //Token expires after 1 week
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("API_SECRET")))
 
