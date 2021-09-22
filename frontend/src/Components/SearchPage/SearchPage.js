0e8a3f0a8f733d229b54f9e89b4d3d31f068899a
Valentin Zwerschke <v.zwerschke@mail.de>
2021-09-22 18:37:21 +0200

    Style search box

--- frontend/src/Components/SearchPage/SearchPage.js
+++ frontend/src/Components/SearchPage/SearchPage.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react'
 import SideBar from '../Sidebar/SideBar'
+import './SearchPage.css'
 
 function SearchPage() {
 	return (
 		<div>
 			<Fragment>
 			<SideBar />
-			<div id="notfound" className="home_content">
-				
+			<div className="home_content">
+				<div className="search_wrapper">
+					<div className="search_input_box">
+						<input type="text" name="text" placeholder="Search for sheets or composers"/>
+						<i class='bx bx-search-alt-2'></i>
+					</div>
+				</div>
 			</div>
 		</Fragment>
 		</div>
