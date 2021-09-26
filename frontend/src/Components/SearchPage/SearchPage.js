ff22cbf9609f94f749ee3f46b84916735f2abf41
Valentin Zwerschke <v.zwerschke@mail.de>
2021-09-26 10:56:16 +0200

    Add input value methods

--- frontend/src/Components/SearchPage/SearchPage.js
+++ frontend/src/Components/SearchPage/SearchPage.js
@@ -1,8 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import SideBar from '../Sidebar/SideBar'
 import './SearchPage.css'
 
 function SearchPage() {
+	
+	const [searchValue, setSearchValue] = useState("")
+
+	const handleSubmit = () => {
+		console.log(searchValue);
+	}
+	
+	/* Checks if Enter is pressed while in input */
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+      		handleSubmit()
+    	}
+	}
+	
 	return (
 		<div>
 			<Fragment>
@@ -10,8 +24,15 @@ function SearchPage() {
 			<div className="home_content">
 				<div className="search_wrapper">
 					<div className="search_input_box">
-						<input type="text" name="text" placeholder="Search for sheets or composers"/>
-						<i class='bx bx-search-alt-2'></i>
+						<input 
+							type="text" 
+							name="text" 
+							placeholder="Search for sheets or composers" 
+							value={searchValue} 
+							onChange={(e) => setSearchValue(e.target.value)}
+							onKeyDown={handleKeyDown}	
+						/>
+						<i className='bx bx-search-alt-2 cursor' onClick={handleSubmit}></i>
 					</div>
 
 					<div className="result_wrapper">
