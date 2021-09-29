a6e2bd01efdb16ce2b567c07cb89b05950285184
Valentin Zwerschke <v.zwerschke@mail.de>
2021-09-29 18:00:28 +0200

    Remove resultBox and replace it with SheetBoxes

--- frontend/src/Components/SearchPage/ResultBox.js
+++ frontend/src/Components/SearchPage/ResultBox.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import SheetBox from '../SheetsPage/Components/SheetBox';
 
 function ResultBox({ searchResponse }) {
 
@@ -8,11 +9,30 @@ function ResultBox({ searchResponse }) {
 
 	return (
 		<div className="result_wrapper">
+			{searchResponse.length == 0 ? 
+				<NoResults />
+				:
+				<Results searchResponse={searchResponse}/>
+			}
+		
+		</div>
+	);
+}
+
+function NoResults() {
+	return (
 		<div className="span_wrapper">
 			<span>No results found yet...</span>
 		</div>
+  	);
+}
+
+function Results({ searchResponse }) {
+	return (
+		<div className="span_wrapper">
+			<SheetBox sheet={searchResponse[0]}/>
 		</div>
-	);
+	)
 }
 
 export default ResultBox
