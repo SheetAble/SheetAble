209c69338b27f82cfc968b46583dbd9daee5f517
Valentin Zwerschke <v.zwerschke@mail.de>
2021-10-14 17:55:35 +0200

    Add mapping and random color to the tags

--- frontend/src/Components/Sheet/Components/InformationCard.js
+++ frontend/src/Components/Sheet/Components/InformationCard.js
@@ -3,14 +3,34 @@ import './InformationCard.css'
 
 
 function InformationCard({ infoText, tags }) {
+
+  const colors = [
+    "#FF6A6A",
+    "#0DAB76",
+    "#00E5E8",
+    "#5C5D8D",
+    "#99B2DD",
+    "#9381FF",
+    "#B8B8FF",
+  ]; // list of dominant colors for the tags
+
   return (
     <div className="information_card">
       <div className="header_wrapper">
         <h1>Information</h1>
-        <div>
-          <span className="dot" />
-          <span>{tags[0]}</span>
-        </div>
+        {tags.map((tag) => (
+          <div>
+            <span
+              className="dot"
+              style={{
+                backgroundColor:
+                  colors[Math.floor(Math.random() * colors.length)],
+              }}
+            />
+            <span>{tag}</span>
+          </div>
+        ))}
+        <span>&nbsp;&nbsp;</span>
       </div>
       <div className="info_text">
         <span>{infoText}</span>
