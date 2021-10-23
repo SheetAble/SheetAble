798744859d9c701c3579bcee7c410af3bacad423
Valentin Zwerschke <v.zwerschke@mail.de>
2021-10-23 16:34:53 +0200

    Add tags header page section

--- frontend/src/Components/TagsPage/TagsPage.js
+++ frontend/src/Components/TagsPage/TagsPage.js
@@ -3,13 +3,14 @@ import { useParams } from 'react-router';
 import SideBar from '../Sidebar/SideBar'
 import { getTagSheets } from '../../Redux/Actions/dataActions';
 import { connect } from 'react-redux';
+import { dominantColors } from '../../Utils/colors';
+import './TagsPage.css'
 
 
 function TagsPage({ getTagSheets }) {
 	
 	const { tagName } = useParams();	
 	const decoded = decodeURIComponent(tagName)
-	console.log(decoded);
 
 	const [sheets, setSheets ] = useState([])
 
@@ -20,12 +21,28 @@ function TagsPage({ getTagSheets }) {
 	}, [])
 
 	return (
-		<Fragment>
-			<SideBar />
-			<div className="home_content">
-				<h1>{decoded}</h1>
-			</div>
-    	</Fragment>
+    <Fragment>
+      <SideBar />
+      <div className="home_content tags_page">
+        <div className="header">
+			<span
+			className="dot"
+			style={{
+				backgroundColor:
+				dominantColors[Math.floor(Math.random() * dominantColors.length)],
+			}}
+			/>
+			<h1>{decoded}</h1>
+		</div>
+        {sheets.length != 0 ? (
+          <div>
+            <span>{sheets[0].safe_sheet_name}</span>
+          </div>
+        ) : (
+          <p>loading</p>
+        )}
+      </div>
+    </Fragment>
   );
 }
 
