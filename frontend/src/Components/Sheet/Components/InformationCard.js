f51b38f8c08115cad00a8a37cacd78501198abc4
Valentin Zwerschke <v.zwerschke@mail.de>
2021-10-24 12:44:53 +0200

    Add add button

--- frontend/src/Components/Sheet/Components/InformationCard.js
+++ frontend/src/Components/Sheet/Components/InformationCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './InformationCard.css'
 import { dominantColors } from '../../../Utils/colors';
+import { IconButton } from '@material-ui/core';
+import AddIcon from "@material-ui/icons/Add";
 
 function InformationCard({ infoText, tags }) {
 
@@ -9,25 +11,31 @@ function InformationCard({ infoText, tags }) {
       <div className="header_wrapper">
         <h1>Information</h1>
         {tags.map((tag) => (
-          
-            <div>
-              <a href={`/tag/${encodeURIComponent(tag)}`} style={{textDecoration: "none", color: "black"}}>
-                <span
-                  className="dot"
-                  style={{
-                    backgroundColor:
-                      dominantColors[
-                        Math.floor(Math.random() * dominantColors.length)
-                      ],
-                  }}
-                />
+          <div>
+            <a
+              href={`/tag/${encodeURIComponent(tag)}`}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <span
+                className="dot"
+                style={{
+                  backgroundColor:
+                    dominantColors[
+                      Math.floor(Math.random() * dominantColors.length)
+                    ],
+                }}
+              />
 
-                <span>{tag}</span>
-              </a>
-            </div>
-          
+              <span>{tag}</span>
+            </a>
+          </div>
         ))}
         <span>&nbsp;&nbsp;</span>
+        <div className="add">
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </div>
       </div>
       <div className="info_text">
         <span>{infoText}</span>
