5f576c12fc67b9d7edb2b01bd0347672204bab59
Valentin Zwerschke <v.zwerschke@mail.de>
2021-10-24 12:22:15 +0200

    Change history push to anchor tag link

--- frontend/src/Components/Sheet/Components/InformationCard.js
+++ frontend/src/Components/Sheet/Components/InformationCard.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import './InformationCard.css'
-import { useHistory } from 'react-router'
 import { dominantColors } from '../../../Utils/colors';
 
 function InformationCard({ infoText, tags }) {
-  const history = useHistory()
 
   return (
     <div className="information_card">
       <div className="header_wrapper">
         <h1>Information</h1>
         {tags.map((tag) => (
-          <div className="tag_tooltip" onClick={() => history.push(`/tag/${encodeURIComponent(tag)}`)}>
-            <span
-              className="dot"
-              style={{
-                backgroundColor:
-                  dominantColors[Math.floor(Math.random() * dominantColors.length)],
-              }}
-            />
+          
+            <div>
+              <a href={`/tag/${encodeURIComponent(tag)}`} style={{textDecoration: "none", color: "black"}}>
+                <span
+                  className="dot"
+                  style={{
+                    backgroundColor:
+                      dominantColors[
+                        Math.floor(Math.random() * dominantColors.length)
+                      ],
+                  }}
+                />
 
-            <span>{tag}</span>
-            <span className="tag_tooltiptext">test</span>
-          </div>
+                <span>{tag}</span>
+              </a>
+            </div>
+          
         ))}
         <span>&nbsp;&nbsp;</span>
       </div>
       <div className="info_text">
         <span>{infoText}</span>
-              <br />
+        <br />
         <div className="tag_tooltip">
           <span>test</span>
-          
+
           <span className="tag_tooltiptext">test</span>
         </div>
       </div>
