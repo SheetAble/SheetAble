6782c502727c5c1bd374ff525568b7adb3babf20
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-23 12:24:03 +0200

    Add card display for forgot password

--- frontend/src/Components/Authentication/ForgotPassword.js
+++ frontend/src/Components/Authentication/ForgotPassword.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './ForgotPassword.css'
+import TextField from "@material-ui/core/TextField";
+import { Button } from '@material-ui/core';
 
 export default function ForgotPassword() {
   return (
-	<div>ForgotPassword</div>
+	<div className='forgot-password-container'>
+    <div className='card'>
+      <h1>Forgot your password?</h1>
+      <h2>No problem, request a reset to your email here:</h2>
+      <TextField id="standard-basic" label="Email" variant="standard" className='email-input'/>
+      <div className='btn-container'>
+        <Button variant='contained' className='btn'>
+          Send Email
+        </Button>  
+      </div>
+    </div>
+  </div>
   )
 }
