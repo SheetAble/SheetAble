60c1621b74cfe62b897962f7dcffd99934d7cf2a
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-23 12:24:26 +0200

    Add disabled and form handlers

--- frontend/src/Components/Authentication/ForgotPassword.js
+++ frontend/src/Components/Authentication/ForgotPassword.js
@@ -2,16 +2,19 @@ import React from 'react'
 import './ForgotPassword.css'
 import TextField from "@material-ui/core/TextField";
 import { Button } from '@material-ui/core';
+import { useState } from 'react';
 
 export default function ForgotPassword() {
+  const [emailValue, setEmailValue] = useState("")
+  // TODO: check if email is valid
   return (
 	<div className='forgot-password-container'>
     <div className='card'>
       <h1>Forgot your password?</h1>
       <h2>No problem, request a reset to your email here:</h2>
-      <TextField id="standard-basic" label="Email" variant="standard" className='email-input'/>
+      <TextField id="standard-basic" label="Email" variant="standard" className='email-input' type='email' value={emailValue} onChange={event => setEmailValue(event.target.value)}/>
       <div className='btn-container'>
-        <Button variant='contained' className='btn'>
+        <Button variant='contained' className='btn' disabled={emailValue == ""} onClick={() => console.log('Submit')}>
           Send Email
         </Button>  
       </div>
