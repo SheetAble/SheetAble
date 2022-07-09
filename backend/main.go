db34f3599a0baf885088c872d4f1b7f11d5ffc41
Valentin Zwerschke <v.zwerschke@mail.de>
2022-07-09 13:53:11 +0200

    Make new build

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/SheetAble/SheetAble/backend/api"
 	"github.com/SheetAble/SheetAble/backend/api/utils"
 )
 
 func main() {
-	version, err := utils.GetLatestTagFromRepository()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	utils.Version = version
+	utils.Version = "v0.8"
 	utils.PrintAsciiVersion()
 	api.Run()
 }
